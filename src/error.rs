fdfe126fa53f4c1c0a3a1269ca144e410343a48d
lur1an <lurian-code@protonmail.com>
2024-07-30 10:54:08 +0200

    WIP on amqp-worker framework

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,26 @@
+use tower::BoxError;
+
 use crate::{AMQPTask, AMQPTaskResult};
 
+/// Error that can happen when publishing the result of a task
 #[derive(Debug, thiserror::Error)]
-pub enum Error {
+pub enum PublishError<T: AMQPTaskResult> {
     #[error(transparent)]
     Lapin(#[from] lapin::Error),
+
     #[error(transparent)]
-    Decode(anyhow::Error),
+    Encode(T::EncodeError),
+}
+
+/// Error that can be returned from the handler of a AMQPTask
+#[derive(Debug, thiserror::Error)]
+pub enum HandlerError<T: AMQPTask> {
     #[error(transparent)]
-    Encode(anyhow::Error),
-    #[error("Woker timed out too often")]
-    Timeout,
-    #[error("Worker failed too often")]
-    WorkerFailure,
+    Lapin(#[from] lapin::Error),
+
+    #[error(transparent)]
+    Publish(PublishError<T::TaskResult>),
+
+    #[error(transparent)]
+    Service(#[from] BoxError),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,69 @@
 pub use lapin;
-use tower::Service;
+
+use lapin::types::FieldTable;
+use tower::{BoxError, Layer, Service};
 
 mod error;
 mod worker;
 
+use std::convert::Infallible;
 use std::fmt::Debug;
-use std::time::Duration;
+use std::future::Future;
 
 use lapin::message::Delivery;
-use lapin::options::{BasicAckOptions, BasicNackOptions, BasicPublishOptions};
+use lapin::options::{BasicAckOptions, BasicConsumeOptions, BasicNackOptions, BasicPublishOptions};
 use lapin::protocol::basic::AMQPProperties;
-use tokio::time::timeout;
 use tokio_stream::StreamExt;
 
-use self::error::Error;
+use self::error::{HandlerError, PublishError};
 
-pub trait AMQPTaskResult {
-    type EncodeError: std::error::Error + Send + Sync + 'static;
+pub trait AMQPTaskResult: Sized + Send + Debug {
+    type EncodeError: std::error::Error + Send + Sync + 'static + Debug;
 
     /// Encode the task result into a byte slice, this allows tasks to use different serialization formats
-    fn encode(&self) -> Result<Vec<u8>, Self::EncodeError>;
+    fn encode(self) -> Result<Vec<u8>, Self::EncodeError>;
 
     fn publish_exchange() -> &'static str;
     fn publish_routing_key() -> &'static str;
+
+    fn publish(
+        self,
+        channel: &lapin::Channel,
+    ) -> impl Future<Output = Result<(), PublishError<Self>>> + Send {
+        async {
+            let payload = self.encode().map_err(PublishError::Encode)?;
+            channel
+                .basic_publish(
+                    Self::publish_exchange(),
+                    Self::publish_routing_key(),
+                    BasicPublishOptions::default(),
+                    &payload,
+                    AMQPProperties::default(),
+                )
+                .await?;
+            Ok(())
+        }
+    }
+}
+
+impl AMQPTaskResult for () {
+    type EncodeError = Infallible;
+
+    fn encode(self) -> Result<Vec<u8>, Self::EncodeError> {
+        unreachable!()
+    }
+
+    fn publish_exchange() -> &'static str {
+        unreachable!()
+    }
+
+    fn publish_routing_key() -> &'static str {
+        unreachable!()
+    }
+
+    async fn publish(self, _channel: &lapin::Channel) -> Result<(), PublishError<Self>> {
+        Ok(())
+    }
 }
 
 pub trait AMQPTask: Sized + Debug {
@@ -34,41 +75,63 @@ pub trait AMQPTask: Sized + Debug {
     fn queue() -> &'static str;
 }
 
-#[derive(Debug)]
-pub struct AMQPWorker<T, H> {
+#[derive(Debug, Clone)]
+pub struct WorkerConfig {
+    /// - If true, the worker will ack a message if it fails to decode the task, use this option
+    ///  if you are sure that its a publisher error.
+    /// - If false, the worker will nack a message if it fails to decode the task, use this option
+    ///  if you might be attaching the worker to the wrong queue or your decoding logic is faulty.
+    pub ack_on_decode_error: bool,
+    pub terminate_on_delivery_error: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct AMQPWorker<T, S> {
     consumer_tag: String,
-    task_handler: H,
+    service: S,
     channel: lapin::Channel,
-    handler_timeout: Duration,
-    max_timeouts: usize,
-    max_failures: usize,
+    config: WorkerConfig,
     _phantom: std::marker::PhantomData<T>,
 }
 
-impl<T, H> AMQPWorker<T, H>
+impl<T, S> AMQPWorker<T, S>
 where
     T: AMQPTask + Send + 'static,
-    H: Service<T> + Send + 'static,
-    <H as Service<T>>::Response: AMQPTaskResult,
-    <H as Service<T>>::Error: std::error::Error,
+    S: Service<T> + Send + 'static + Clone,
+    <S as Service<T>>::Response: AMQPTaskResult,
+    <S as Service<T>>::Error: Debug + Into<BoxError>,
 {
     pub fn new(
-        name: String,
-        task_handler: H,
+        consumer_tag: impl Into<String>,
+        service: S,
         channel: lapin::Channel,
-        handler_timeout: Duration,
-        max_timeouts: usize,
-        max_failures: usize,
+        config: WorkerConfig,
     ) -> Self {
         Self {
-            consumer_tag: name,
-            task_handler,
+            consumer_tag: consumer_tag.into(),
+            service,
+            config,
             channel,
-            handler_timeout,
-            max_timeouts,
-            max_failures,
             _phantom: std::marker::PhantomData,
         }
+    }
+
+    pub fn with_consumer_tag(self, consumer_tag: impl Into<String>) -> Self {
+        Self {
+            consumer_tag: consumer_tag.into(),
+            ..self
+        }
+    }
+
+    pub fn add_layer<L>(self, layer: L) -> AMQPWorker<T, L::Service>
+    where
+        L: Layer<S>,
+        <L as Layer<S>>::Service: Service<T> + Send + 'static + Clone,
+        <<L as Layer<S>>::Service as Service<T>>::Error: Debug + Into<BoxError>,
+        <<L as Layer<S>>::Service as Service<T>>::Response: AMQPTaskResult,
+    {
+        let service = layer.layer(self.service);
+        AMQPWorker::new(self.consumer_tag, service, self.channel, self.config)
     }
 
     /// Additional function to add an asynchronous tracing span on top for handling of a delivery
@@ -76,99 +139,106 @@ where
         skip(self, delivery),
         fields(
             consumer_tag = %self.consumer_tag,
-            delivery_tag = %delivery.delivery_tag,
+            delivery_tag = delivery.delivery_tag,
             exchange = %delivery.exchange,
             routing_key = %delivery.routing_key
         )
     )]
-    async fn handle_delivery(
-        &mut self,
-        delivery: &Delivery,
-        timeouts: &mut usize,
-        failures: &mut usize,
-    ) -> Result<(), Error> {
+    async fn handle_delivery(&mut self, delivery: &Delivery) -> Result<(), HandlerError<T>> {
         let task = match T::decode(&delivery.data) {
             Ok(task) => task,
             Err(e) => {
-                tracing::error!(worker_name = self.consumer_tag, ?e, "Failed to decode task");
+                tracing::error!(consumer = self.consumer_tag, ?e, "Failed to decode task");
                 delivery.ack(BasicAckOptions::default()).await?;
                 return Ok(());
             }
         };
-        tracing::info!(worker_name = self.consumer_tag, ?task, "Received task");
-        let handler_fut = self.task_handler.call(task);
-        let handler_result = match timeout(self.handler_timeout, handler_fut).await {
-            Ok(result) => result,
+        tracing::info!(consumer = self.consumer_tag, ?task, "Received task");
+        let task_result = self.service.call(task).await.map_err(Into::into)?;
+        //let task_result = match handler_result {
+        //    Ok(task_result) => task_result,
+        //    Err(e) => {
+        //        tracing::error!(self.consumer_tag, ?e, "Task handler failed");
+        //        delivery
+        //            .nack(BasicNackOptions {
+        //                multiple: false,
+        //                requeue: true,
+        //            })
+        //            .await?;
+        //        *failures += 1;
+        //        if *failures >= self.max_failures {
+        //            return Err(Error::WorkerFailure);
+        //        } else {
+        //            return Ok(());
+        //        }
+        //    }
+        //};
+        match task_result.publish(&self.channel).await {
+            Ok(_) => {
+                delivery.ack(BasicAckOptions::default()).await?;
+            }
             Err(e) => {
-                tracing::error!(self.consumer_tag, ?e, "Task handler timed out");
+                tracing::error!(self.consumer_tag, ?e, "Publish failed for task result");
                 delivery
                     .nack(BasicNackOptions {
                         multiple: false,
                         requeue: true,
                     })
                     .await?;
-                *timeouts += 1;
-                if *timeouts >= self.max_timeouts {
-                    return Err(Error::Timeout);
-                }
-                return Ok(());
             }
-        };
-        match handler_result {
-            Ok(task_result) => {
-                let payload = task_result.encode().map_err(|e| Error::Encode(e.into()))?;
-                self.channel
-                    .basic_publish(
-                        T::TaskResult::publish_exchange(),
-                        T::TaskResult::publish_routing_key(),
-                        BasicPublishOptions::default(),
-                        &payload,
-                        AMQPProperties::default(),
-                    )
-                    .await?;
-                delivery.ack(BasicAckOptions::default()).await?;
-            }
-            Err(e) => {
-                tracing::error!(self.consumer_tag, ?e, "Task handler failed");
-                delivery
-                    .nack(BasicNackOptions {
-                        multiple: false,
-                        requeue: true,
-                    })
-                    .await?;
-                *failures += 1;
-                if *failures >= self.max_failures {
-                    return Err(Error::WorkerFailure);
-                }
-            }
-        };
+        }
         Ok(())
     }
 
-    /// Start consuming from the embedded `Consumer`.
-    /// This function cannot return Ok(()) as its supposed to run indefinitely
-    /// # Errors
-    /// - If the *AMQP* stream ends (it should continue indefinitely) the function will return an error
-    /// - If a `nack` or a `ack` operation fails the consumer will return the Error
-    /// - If the handler times out `max_timeouts` times the function will return an error
-    /// - If the handler returns an error `max_failures` times the function will return an error
-    /// - If encoding task result fails the function exits, this shouldn't happen.
-    /// - If publishing a result fails the function exits with error.
-    pub async fn consume(&mut self) -> Result<(), Error> {
+    pub async fn consume(
+        mut self,
+        consume_options: BasicConsumeOptions,
+        consume_arguments: FieldTable,
+    ) -> Result<(), lapin::Error> {
         tracing::info!(name = self.consumer_tag, "Starting worker consumer");
-        let mut timeouts = 0;
-        let mut failures = 0;
-        let consumer: lapin::Consumer = todo!();
+        let mut consumer = self
+            .channel
+            .basic_consume(
+                T::queue(),
+                &self.consumer_tag,
+                consume_options,
+                consume_arguments,
+            )
+            .await?;
         while let Some(attempted_delivery) = consumer.next().await {
             let delivery = match attempted_delivery {
                 Ok(delivery) => delivery,
                 Err(e) => {
                     tracing::error!(name = self.consumer_tag, ?e, "Delivery of message failed");
+                    if self.config.terminate_on_delivery_error {
+                        return Ok(());
+                    }
                     continue;
                 }
             };
-            self.handle_delivery(&delivery, &mut timeouts, &mut failures)
-                .await?;
+            let task = match T::decode(&delivery.data) {
+                Ok(task) => task,
+                Err(e) => {
+                    tracing::error!(consumer = self.consumer_tag, ?e, "Failed to decode task");
+                    if self.config.ack_on_decode_error {
+                        delivery.ack(BasicAckOptions::default()).await?;
+                    }
+                    continue;
+                }
+            };
+            match self.handle_delivery(&delivery).await {
+                Ok(_) => {
+                    tracing::info!(
+                        consumer = self.consumer_tag,
+                        delivery_tag = delivery.delivery_tag,
+                        "Delivery handled successfully"
+                    );
+                    continue;
+                }
+                Err(HandlerError::Service(e)) => todo!(),
+                Err(HandlerError::Publish(e)) => todo!(),
+                Err(HandlerError::Lapin(e)) => todo!(),
+            }
         }
         unreachable!()
     }
